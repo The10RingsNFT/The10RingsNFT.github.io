fa7ba76604614e71f55c3ea9b4376a24e1ada9f0
dylviz <dylanvizcarra@gmail.com>
2022-01-14 23:30:38 -0800

    updated favicon, updated timer

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2022/01/15';
+        var finalDate = '2022/01/27';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
