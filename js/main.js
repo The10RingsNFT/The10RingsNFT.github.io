2291ed4da2b8e135773ec16c30f27cf9c10d42f9
dylviz <dylanvizcarra@gmail.com>
2022-02-19 14:06:40 -0800

    Updated mint date

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2022/02/16';
+        var finalDate = '2022/03/02';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
