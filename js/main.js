82d51c99277d967f6826beac67cc6e3c19e2e965
dylviz <dylanvizcarra@gmail.com>
2022-03-01 21:57:14 -0800

    Updated clock for 2nd ring

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2022/03/02';
+        var finalDate = '2022/03/14';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
