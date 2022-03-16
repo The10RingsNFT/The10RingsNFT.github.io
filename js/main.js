171fb43ff7745f58520b6d3919a1b9893962f333
dylviz <dylanvizcarra@gmail.com>
2022-03-15 22:18:49 -0700

    updated riddle

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2022/03/21';
+        var finalDate = '2022/03/26';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
